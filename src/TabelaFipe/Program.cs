f5e39170adc20c95a64a221e5c5bcef304c6c787
Jorge Soprani <jorgesoprani@gmail.com>
2024-03-27 11:35:17 -0300

    Added working project

---
+++ src/TabelaFipe/Program.cs
@@ -0,0 +1,222 @@
+﻿using TabelaFipe;
+var service = new FipeService();
+
+var anoAtual = "32000";
+var gasolina = "1";
+var etanol = "2";
+var diesel = "3";
+var tiposCombustivel = new[] { gasolina, etanol, diesel };
+var tabelas = await service.ConsultarTabelaDeReferenciaAsync();
+var tabela = PromptTabelaReferencia(tabelas);
+var ultimoMes = tabela.Codigo;
+
+Console.WriteLine($"A partir de que ano?");
+var anoInicial = Console.ReadLine();
+
+var anoFinal = int.Parse(tabela.Mes.Split("/")[1]);
+
+var filtroAnos = new List<string>();
+for (int i = int.Parse(anoInicial); i <= anoFinal + 1; i++)
+{
+    filtroAnos.Add(i.ToString());
+}
+filtroAnos.Add(anoAtual);
+
+Console.WriteLine($"Buscando dados de modelos entre ano {anoInicial} e {anoFinal}");
+
+var porCodigo = false;
+
+Console.WriteLine($"Buscar por lista de codigos FIPE? S para sim");
+porCodigo = Console.ReadKey().Key == ConsoleKey.S;
+
+var batchSize = 100;
+if (porCodigo)
+{
+    List<string> codigos = GetCodigos();
+    var combos = new List<(string codigo, string ano, string tipoCombustivel)>();
+    List<Carro> carros = new List<Carro>();
+    foreach (var codigo in codigos)
+    {
+        foreach (var ano in filtroAnos)
+        {
+            foreach (var tipoCombustivel in tiposCombustivel)
+            {
+                combos.Add((codigo, ano, tipoCombustivel));
+            }
+        }
+    }
+
+    var count = 0;
+    var batches = new List<Task>();
+    foreach (var item in combos)
+    {
+        count++;
+        batches.Add(Task.Run(async () =>
+        {
+            //Console.WriteLine($"({item.ano} - {item.codigo} - {item.tipoCombustivel})");
+            var carro = await service.ConsultarCarroPorCodigo(
+                ultimoMes,
+                item.codigo,
+                item.ano + "-" + item.tipoCombustivel);
+
+            if (carro != null && carro.Modelo != null)
+            {
+                //Console.WriteLine($"{carro.Marca} - {carro.Modelo} - {carro.Valor}");
+                carros.Add(carro);
+            }
+        }));
+
+        if (batches.Count == batchSize || combos.IndexOf(item) == (combos.Count - 1))
+        {
+            Console.WriteLine($"Item {count} de {combos.Count}");
+            Task.WaitAll(batches.ToArray());
+            batches.Clear();
+        }
+    }
+    WriteOutput(carros);
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine($"Buscando opções por cada ano...");
+    var combos = new List<(int marca, string codigo, string ano, string tipoCombustivel)>();
+    var marcas = await service.ConsultarMarcas(ultimoMes);
+    var batches = new List<Task>();
+    foreach (var marca in marcas)
+    {
+        foreach (var ano in filtroAnos)
+        {
+            foreach (var tipoCombustivel in tiposCombustivel)
+            {
+                batches.Add(Task.Run(async () =>
+                {
+                    var modelos = await service.ConsultarModelosPorAno(ultimoMes, marca.Value, ano + "-" + tipoCombustivel);
+
+                    foreach (var modelo in modelos)
+                    {
+                        combos.Add((marca.Value, modelo.Value, ano, tipoCombustivel));
+                    }
+                }));
+
+                if (batches.Count == batchSize)
+                {
+                    Task.WaitAll(batches.ToArray());
+                    batches.Clear();
+                }
+            }
+        }
+    }
+
+    if (batches.Any())
+    {
+        Task.WaitAll(batches.ToArray());
+        batches.Clear();
+    }
+    Console.WriteLine($"Buscando valores...");
+    List<Carro> carros = new List<Carro>();
+    var count = 0;
+    foreach (var item in combos)
+    {
+        count++;
+        batches.Add(Task.Run(async () =>
+        {
+            //Console.WriteLine($"({item.ano} - {item.codigo} - {item.tipoCombustivel})");
+            var carro = await service.ConsultarCarro(
+                ultimoMes,
+                item.marca,
+                item.codigo,
+                item.ano + "-" + item.tipoCombustivel);
+
+            if (carro != null && carro.Modelo != null)
+            {
+                //Console.WriteLine($"{carro.Marca} - {carro.Modelo} - {carro.Valor}");
+                carros.Add(carro);
+            }
+        }));
+
+        if (batches.Count == batchSize || combos.IndexOf(item) == (combos.Count - 1))
+        {
+            Console.WriteLine($"Item {count} de {combos.Count}");
+            Task.WaitAll(batches.ToArray());
+            batches.Clear();
+        }
+    }
+    WriteOutput(carros);
+}
+
+static void WriteOutput(List<Carro> carros)
+{
+    var output = new List<string>();
+    output.Add($"Tabela de Referencia,Marca,Modelo,Ano,Codigo Fipe,Combustivel,Valor");
+    foreach (var carro in carros)
+    {
+        output.Add($"{carro.MesReferencia},{carro.Marca},\"{carro.Modelo.Replace(",", " ")}\",{(carro.AnoModelo == 32000 ? "Ano atual" : carro.AnoModelo)},{carro.CodigoFipe},{(carro.Combustivel)},\"{carro.Valor.Replace("R$ ", "")}\"");
+    }
+
+    File.WriteAllLines("Resultado.csv", output);
+
+}
+
+static List<string> GetCodigos()
+{
+    var localArquivo = "Codigos.csv";
+
+    Console.WriteLine($"Procurando arquivo na pasta atual com nome {localArquivo}. Digite caminho diferente se necessário");
+    var novoCaminho = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(novoCaminho))
+    {
+        localArquivo = novoCaminho;
+    }
+
+    var arquivo = File.ReadAllLines(localArquivo);
+
+    var codigos = new List<string>();
+    for (int i = 1; i < arquivo.Length; i++)
+    {
+        var line = arquivo[i];
+        if (line.StartsWith("\""))
+        {
+            codigos.Add(line.Split("\"").Last().Replace(",", ""));
+        }
+        else
+        {
+            codigos.Add(line.Split(",").Last());
+        }
+    }
+
+    return codigos;
+}
+
+static TabelaReferencia PromptTabelaReferencia(List<TabelaReferencia> tabelas)
+{
+    TabelaReferencia result = null;
+    var atual = tabelas.First();
+    while (result == null)
+    {
+        Console.WriteLine($"Qual data de referencia utilizar? (ex: junho/2023) deixe vazio para usar a atual({atual.Mes})");
+        var input = Console.ReadLine();
+        if (input == "")
+        {
+            result = atual;
+            return atual;
+        }
+
+        var tabelaSelecionada = tabelas.FirstOrDefault(x => x.Mes.Trim().Equals(input?.Trim(), StringComparison.CurrentCultureIgnoreCase));
+
+        if (tabelaSelecionada == null)
+        {
+            Console.WriteLine($"Tabela não encontrada. Essas são as tabelas disponíveis:");
+            foreach (var tabela in tabelas)
+            {
+                Console.WriteLine($"{tabela.Mes}");
+            }
+        }
+        else
+        {
+            result = tabelaSelecionada;
+            return tabelaSelecionada;
+        }
+    }
+
+    return result;
+}
---
+++ src/TabelaFipe/Service.cs
@@ -0,0 +1,202 @@
+﻿using System.Net.Http.Json;
+
+namespace TabelaFipe
+{
+    public class FipeService
+    {
+        private HttpClient _httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri("https://veiculos.fipe.org.br")
+        };
+
+        public async Task<List<TabelaReferencia>> ConsultarTabelaDeReferenciaAsync()
+        {
+            var response = await _httpClient.PostAsync("/api/veiculos/ConsultarTabelaDeReferencia", new StringContent(""));
+            return await response.Content.ReadFromJsonAsync<List<TabelaReferencia>>();
+        }
+
+        public async Task<List<Marca>> ConsultarMarcas(int codigoTabela, int tipoVeiculo = 1)
+        {
+            var response = await _httpClient.PostAsync("/api/veiculos/ConsultarMarcas", new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "codigoTabelaReferencia", codigoTabela.ToString() },
+                { "codigoTipoVeiculo", tipoVeiculo.ToString() },
+            }));
+            if (!response.IsSuccessStatusCode)
+            {
+                return new List<Marca>();
+            }
+            return await response.Content.ReadFromJsonAsync<List<Marca>>();
+        }
+
+        public async Task<PesquisaModelo> ConsultarModelos(int codigoTabela, int marca, int tipoVeiculo = 1)
+        {
+            var contentData = new Dictionary<string, string>
+            {
+                { "codigoTabelaReferencia", codigoTabela.ToString() },
+                { "codigoTipoVeiculo", tipoVeiculo.ToString() },
+                { "codigoMarca", marca.ToString() },
+            };
+
+            var content = new FormUrlEncodedContent(contentData);
+            var response = await _httpClient.PostAsync("/api/veiculos/ConsultarModelos", content);
+            if (!response.IsSuccessStatusCode)
+            {
+                return new PesquisaModelo { Anos = new List<Ano>(), Modelos = new List<Modelo>() };
+            }
+            return await response.Content.ReadFromJsonAsync<PesquisaModelo>();
+        }
+
+        public async Task<List<ModeloAno>> ConsultarModelosPorAno(int codigoTabela, int marca, string ano, int tipoVeiculo = 1)
+        {
+            try
+            {
+                var contentData = new Dictionary<string, string>
+            {
+                { "codigoTabelaReferencia", codigoTabela.ToString() },
+                { "codigoTipoVeiculo", tipoVeiculo.ToString() },
+                { "codigoMarca", marca.ToString() },
+                { "ano", ano },
+                { "anoModelo", ano.Split("-")[0] },
+                { "codigoTipoCombustivel", ano.Split("-")[1] },
+            };
+
+                var content = new FormUrlEncodedContent(contentData);
+                var response = await _httpClient.PostAsync("/api/veiculos/ConsultarModelosAtravesDoAno", content);
+                var json = await response.Content.ReadAsStringAsync();
+                if (json.Contains("nadaencontrado"))
+                {
+                    return new List<ModeloAno>();
+                }
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new List<ModeloAno>();
+                }
+                return await response.Content.ReadFromJsonAsync<List<ModeloAno>>();
+
+            }
+            catch (Exception ex)
+            {
+                return new List<ModeloAno>();
+            }
+        }
+
+        public async Task<Carro> ConsultarCarro(int codigoTabela, int marca, string codigoModelo, string anoModelo, int tipoVeiculo = 1)
+        {
+            try
+            {
+                var contentData = new Dictionary<string, string>
+                {
+                    { "codigoTabelaReferencia", codigoTabela.ToString() },
+                    { "codigoTipoVeiculo", tipoVeiculo.ToString() },
+                    { "codigoMarca", marca.ToString() },
+                    { "codigoModelo", codigoModelo },
+                    { "anoModelo", anoModelo.Split("-")[0] },
+                    { "codigoTipoCombustivel", anoModelo.Split("-")[1] },
+                    { "tipoVeiculo", "carro" },
+                    { "tipoConsulta", "tradicional" },
+                };
+
+                var content = new FormUrlEncodedContent(contentData);
+                var response = await _httpClient.PostAsync("/api/veiculos/ConsultarValorComTodosParametros", content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+
+                var result = await response.Content.ReadFromJsonAsync<Carro>();
+                if (result.Codigo == "0")
+                {
+                    return null;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<Carro> ConsultarCarroPorCodigo(int codigoTabela, string codigoFipe, string anoModelo, int tipoVeiculo = 1)
+        {
+            try
+            {
+                var contentData = new Dictionary<string, string>
+                {
+                    { "codigoTabelaReferencia", codigoTabela.ToString() },
+                    { "codigoTipoVeiculo", tipoVeiculo.ToString() },
+                    { "anoModelo", anoModelo.Split("-")[0] },
+                    { "codigoTipoCombustivel", anoModelo.Split("-")[1] },
+                    { "tipoVeiculo", "carro" },
+                    { "tipoConsulta", "codigo" },
+                    { "modeloCodigoExterno", codigoFipe }
+                };
+
+                var content = new FormUrlEncodedContent(contentData);
+                var response = await _httpClient.PostAsync("/api/veiculos/ConsultarValorComTodosParametros", content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    return null;
+                }
+                var result = await response.Content.ReadFromJsonAsync<Carro>();
+                if (result.Codigo == "0")
+                {
+                    return null;
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+
+    public class TabelaReferencia
+    {
+        public int Codigo { get; set; }
+        public string Mes { get; set; }
+    }
+
+    public class Marca
+    {
+        public int Value { get; set; }
+        public string Label { get; set; }
+    }
+
+    public class PesquisaModelo
+    {
+        public List<Ano> Anos { get; set; }
+        public List<Modelo> Modelos { get; set; }
+    }
+
+    public class Ano
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Modelo
+    {
+        public string Label { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class ModeloAno
+    {
+        public string Label { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class Carro
+    {
+        public string Codigo { get; set; }
+        public int AnoModelo { get; set; }
+        public string CodigoFipe { get; set; }
+        public string Combustivel { get; set; }
+        public string Marca { get; set; }
+        public string MesReferencia { get; set; }
+        public string Modelo { get; set; }
+        public string Valor { get; set; }
+    }
+}
